b10d87c786394cca07ce3431a224d5d3ad57c822
sidk003 <31365123+sidk003@users.noreply.github.com>
2020-05-10 23:58:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import Sorter from "./components/sorter";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Sorter></Sorter>
+      </div>
+    );
+  }
+}
+
+export default App;
