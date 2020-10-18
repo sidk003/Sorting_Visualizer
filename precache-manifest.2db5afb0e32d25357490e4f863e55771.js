4049baf1b546d4240e6732fd87752310d44456f6
sidk003 <siddhu1583@gmail.com>
2020-10-18 22:23:02 +0530

    Updates

--- precache-manifest.2db5afb0e32d25357490e4f863e55771.js
+++ precache-manifest.2db5afb0e32d25357490e4f863e55771.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6c00bd29b02c20abbb04d5a86a6f4522",
+    "revision": "4179e1972a1f769498ed233ddc7c43d6",
     "url": "/Sorting_Visualizer/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Sorting_Visualizer/static/css/2.67123b20.chunk.css"
   },
   {
-    "revision": "5c6410f8e7a8912ea520",
-    "url": "/Sorting_Visualizer/static/css/main.b1a62ba2.chunk.css"
+    "revision": "8b6c045ee7e70b3592f2",
+    "url": "/Sorting_Visualizer/static/css/main.d4b9892e.chunk.css"
   },
   {
     "revision": "ae9eb5d9d73ec77366df",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Sorting_Visualizer/static/js/2.efead2ef.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5c6410f8e7a8912ea520",
-    "url": "/Sorting_Visualizer/static/js/main.99099153.chunk.js"
+    "revision": "8b6c045ee7e70b3592f2",
+    "url": "/Sorting_Visualizer/static/js/main.34bbfa64.chunk.js"
   },
   {
     "revision": "1167647d9f69dcae96d8",
